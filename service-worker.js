7c8cf7addbd31242aca71a63a1310202806d9d47
jbr284 <jbrosa2009@gmail.com>
2025-07-11 20:29:10 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ self.addEventListener('install', function(event) {
         '/',
         '/index.html',
         '/manifest.json',
-        '/icons/icon-192x192.png',
-        '/icons/icon-512x512.png'
+        '/icon-192x192.png',
+        '/icon-512x512.png'
       ]);
     })
   );
